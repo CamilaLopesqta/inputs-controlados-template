c158d9447e8658f15b30b66b29ba0547b29a0d7a
Camila Lopes <camilalopesqta@gmail.com>
2023-08-16 21:16:59 -0300

    Exercícios da aula de Inputs Controlados

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -1,23 +1,78 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { MainContainer, Form, Input } from './styles'
 
 function MainPage() {
-  return (
+  const [nome, setNome] = useState("") 
+  const [idade, setIdade] = useState("")
+  const [estadoCivil, setEstadoCivil] = useState("")
+  const [email, setEmail] = useState ("")
+  const [senha, setSenha] = useState("")
+
+  const alterarNome = (event) => {
+    setNome(event.target.value)
+  }
+
+  const alterarIdade = (event) => {
+    setIdade(event.target.value)
+  }
+
+  const alterarEstadoCivil = (event) => {
+    setEstadoCivil(event.target.value)
+  }
+
+  const limparInputs = (event) => {
+    event.preventDefault()
+      setNome("")
+      setIdade("")
+      setEstadoCivil("")
+      setEmail("")
+      setSenha("")
+  };
+  
+  const alterarEmail = (event) => {
+    setEmail(event.target.value)
+  }
+
+  const alterarSenha = (event) => {
+    setSenha(event.target.value)
+  }
+  console.log(nome, idade, estadoCivil, email, senha)
+
+   return (
     <MainContainer>
       <h2>Formulário de inscrição</h2>
       <Form>
         <label>
           Nome:
-          <Input />
+          <Input value={nome} onChange={alterarNome}/>
         </label>
         <label>
           Idade:
-          <Input />
+          <Input value={idade} onChange={alterarIdade}/>
         </label>
-        <button>Enviar dados</button>
+        <label>
+          Estado civil:
+          <br/>
+          <select value={estadoCivil} onChange={alterarEstadoCivil}>
+            <option>Nenhum</option>
+            <option>Solteiro(a)</option>
+            <option>Casado(a)</option>
+            <option>Viúvo(a)</option>
+            <option>Divorciado(a)</option>
+          </select>
+        </label>
+        <label>
+          E-mail:
+          <Input value={email} onChange={alterarEmail}/>
+        </label>
+        <br/>
+        <label>
+          Senha:
+          <Input value={senha} onChange={alterarSenha}/>
+        </label>
+        <button onClick={limparInputs}>Enviar dados</button>
       </Form>
     </MainContainer>
   )
-}
-
+   }
 export default MainPage
